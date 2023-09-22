ee223a3ecf035595fa9d53f728e18d392777c63b
Pashcekvich227 <iport234@gmail.com>
2023-09-22 23:45:52 +0300

    feat: negative tests pizza

--- tests/test_pizza.py
+++ tests/test_pizza.py
@@ -3,6 +3,7 @@ from httpx import AsyncClient
 from conftest import pizza
 
 
+# Create pizza
 @pytest.mark.run(order=8)
 async def test_create_pizza(ac: AsyncClient):
     token = ac.cookies.get("User")
@@ -10,6 +11,16 @@ async def test_create_pizza(ac: AsyncClient):
     assert response.status_code == 201
 
 
+@pytest.mark.run(order=8)
+async def test_negative_create_pizza(ac: AsyncClient):
+    token = ac.cookies.get("User")
+    response = await ac.post("/pizza/",
+                             json={"name": "Test_pizza"},
+                             headers={"Cookie": f"User={token}"})
+    assert response.status_code == 422
+
+
+# Get pizza
 @pytest.mark.run(order=9)
 async def test_get_pizza_by_id(ac: AsyncClient):
     pizza_id = 1
@@ -18,6 +29,16 @@ async def test_get_pizza_by_id(ac: AsyncClient):
     assert response.status_code == 200
 
 
+@pytest.mark.run(order=9)
+async def test_negative_get_pizza_by_id(ac: AsyncClient):
+    pizza_id = 10
+    token = ac.cookies.get("User")
+    response = await ac.get(f"/pizza/{pizza_id}", headers={"Cookie": f"User={token}"})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Pizza not found'}
+
+
+# Put pizza
 @pytest.mark.run(order=10)
 async def test_put_pizza_by_id(ac: AsyncClient):
     pizza_id = 1
@@ -31,8 +52,25 @@ async def test_put_pizza_by_id(ac: AsyncClient):
     assert response.status_code == 200
 
 
+@pytest.mark.run(order=10)
+async def test_negative_put_pizza_by_id(ac: AsyncClient):
+    pizza_id = 10
+    token = ac.cookies.get("User")
+    response = await ac.put(f"/pizza/{pizza_id}",
+                            json={
+                                "name": "string",
+                                "description": "string",
+                                "size": "S",
+                                "price": 0
+                            },
+                            headers={"Cookie": f"User={token}"})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Pizza not found'}
+
+
+# Get list pizza
 @pytest.mark.run(order=11)
-async def test_put_pizza_by_id(ac: AsyncClient):
+async def test_get_all_pizzas(ac: AsyncClient):
     await test_create_pizza(ac)
     skip = 0
     limit = 100
@@ -42,10 +80,32 @@ async def test_put_pizza_by_id(ac: AsyncClient):
     assert len(response.json()) == 2
 
 
+@pytest.mark.run(order=11)
+async def test_negative_get_all_pizzas(ac: AsyncClient):
+    await test_create_pizza(ac)
+    skip = 0
+    limit = 0
+    token = ac.cookies.get("User")
+    response = await ac.get(f"/pizza/?skip={skip}&limit={limit}", headers={"Cookie": f"User={token}"})
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Pizza not found'}
+
+
+# Delete pizza
 @pytest.mark.run(order=12)
 async def test_delete_pizza_by_id(ac: AsyncClient):
     pizza_id = 2
     token = ac.cookies.get("User")
     response = await ac.delete(f"/pizza/{pizza_id}", headers={"Cookie": f"User={token}"})
     assert response.status_code == 200
+
+
+@pytest.mark.run(order=12)
+async def test_negative_delete_pizza_by_id(ac: AsyncClient):
+    pizza_id = 10
+    token = ac.cookies.get("User")
+    response = await ac.delete(f"/pizza/{pizza_id}", headers={"Cookie": f"User={token}"})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Pizza not found'}
 
